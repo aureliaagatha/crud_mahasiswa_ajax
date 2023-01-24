4a88deb8e627551b9f57767ff2895728f6cff5e7
aureliaagatha <aureliaagatha11@gmail.com>
2023-01-24 10:25:03 +0700

    ubah dikit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost/crud_mahasiswa_ajax';
+$config['base_url'] = 'http://localhost/crud_mahasiswa_ajax';
 
 /*
 |--------------------------------------------------------------------------
